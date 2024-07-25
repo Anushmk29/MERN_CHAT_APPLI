e4093715f57c3a31c0cd441213707e0b80f8978e
Anush MK <anushmk85@gmail.com>
2024-07-26 01:46:46 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -35,19 +35,19 @@ app.use('/api/user',userRoutes);
 app.use('/api/chat',chatRoutes);
 app.use('/api/message',messageRoutes);
 
-//---------------------deploymet--------------
-const __dirname1 =  path.resolve()
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname1,"/frontend/dist")))
+// //---------------------deploymet--------------
+// const __dirname1 =  path.resolve()
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname1,"/frontend/dist")))
 
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname1,"frontend","dist","index.html"))
-    })
-}else{
-    app.get('/',(req,res)=>{
-        res.send("API is running successfully")
-    })
-}
+//     app.get('*',(req,res)=>{
+//         res.sendFile(path.resolve(__dirname1,"frontend","dist","index.html"))
+//     })
+// }else{
+//     app.get('/',(req,res)=>{
+//         res.send("API is running successfully")
+//     })
+// }
 //---------------------------------------
 //error handling middlewares
 app.use(notFound)
