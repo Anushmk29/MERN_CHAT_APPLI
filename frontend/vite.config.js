59b27d65b323e32e37420be344c79b7a0aa7fd95
Anush MK <anushmk85@gmail.com>
2024-07-27 00:33:21 +0530

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: { chunkSizeWarningLimit: 1600, },
    server: {
     proxy: {
       '/api/chat': 'http://localhost:5000', // your backend server URL
