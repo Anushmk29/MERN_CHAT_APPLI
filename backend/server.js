c9bc76833eecc22f5bd60bc36fa1db2fcd96743e
Anush MK <anushmk85@gmail.com>
2024-08-03 22:16:04 +0530

    new commits

--- backend/server.js
+++ backend/server.js
@@ -34,20 +34,20 @@ app.use('/api/chat',chatRoutes);
 app.use('/api/message',messageRoutes);
 
 // //---------------------deploymet--------------
-const __dirname1 =  path.resolve()
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname1,"/frontend/dist")))
+// const __dirname1 =  path.resolve()
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname1,"/frontend/dist")))
 
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname1,"frontend","dist","index.html"))
-    })
-    console.log('__dirname1',__dirname1);
+//     app.get('*',(req,res)=>{
+//         res.sendFile(path.resolve(__dirname1,"frontend","dist","index.html"))
+//     })
+//     console.log('__dirname1',__dirname1);
     
-}else{
-    app.get('/',(req,res)=>{
-        res.send("API is running successfully")
-    })
-}
+// }else{
+//     app.get('/',(req,res)=>{
+//         res.send("API is running successfully")
+//     })
+// }
 //---------------------------------------
 //error handling middlewares
 app.use(notFound)
